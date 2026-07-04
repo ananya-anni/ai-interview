bee14b5670caac67c70d0c300a0220fa9bdb7fef
Ananya Priya <ananyapriya1003@gmail.com>
2026-07-05 00:06:15 +0530

    added logic to form

--- ai-based-interview/src/components/Login.jsx
+++ ai-based-interview/src/components/Login.jsx
@@ -1,13 +1,79 @@
 import Input from "./Input";
+import { useState } from "react";
 const Login = () => {
+  const [formData, setFormData] = useState({
+    userEmail: "",
+    userPassword: "",
+  });
+
+  const [errors, setErrors] = useState({
+    errorEmail: "",
+    errorPassword: "",
+  });
+
+  const validateInputs = () => {
+    let newError = {
+      errorEmail: "",
+      errorPassword: "",
+    };
+
+    const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+    if (formData.userEmail.trim() === "") {
+      newError.errorEmail = "Email is required";
+    } else if (!emailRegex.test(formData.userEmail)) {
+      newError.errorEmail = "Please enter a valid email";
+    }
+    const passwordRegex =
+      /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@.#$!%*?&])[A-Za-z\d@.#$!%*?&]{8,15}$/;
+
+    if (formData.userPassword.trim() === "") {
+      newError.errorPassword = "Password is required";
+    } else if (!passwordRegex.test(formData.userPassword)) {
+      if (
+        formData.userPassword.length < 8 ||
+        formData.userPassword.length > 15
+      ) {
+        newError.errorPassword = "Password must contain 8 to 15 characters.";
+      } else
+        newError.errorPassword = `Password must contain at least 1 lowecase,
+           1 uppercase ,1 digit and 1 special character.`;
+    }
+    setErrors(newError);
+    return newError;
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const newErrorValue = validateInputs();
+    if (Object.values(newErrorValue).every((error) => error === "")) {
+      setFormData({
+        userEmail: "",
+        userPassword: "",
+      });
+    }
+  };
+
+  const handleChange = (event) => {
+    setFormData((currValue) => {
+      const newFormData = {
+        ...currValue,
+        [event.target.name]: event.target.value,
+      };
+      return newFormData;
+    });
+  };
   return (
     <div className="flex flex-col">
-      <form action="" className="flex flex-col gap-3">
+      <form action="" onSubmit={handleSubmit} className="flex flex-col gap-3">
         <Input
           id="user-email"
           label="Email"
           type="email"
           placeholder="Enter your email"
+          inputName="userEmail"
+          value={formData.userEmail}
+          onInputChange={handleChange}
+          error={errors.errorEmail}
         />
 
         <Input
@@ -15,6 +81,10 @@ const Login = () => {
           type="password"
           label="Password"
           placeholder="Enter your password"
+          inputName="userPassword"
+          value={formData.userPassword}
+          onInputChange={handleChange}
+          error={errors.errorPassword}
         />
 
         <button
--- ai-based-interview/src/components/SignUp.jsx
+++ ai-based-interview/src/components/SignUp.jsx
@@ -1,34 +1,143 @@
 import Input from "./Input";
+import { useState } from "react";
 const SignUp = () => {
+  const [formData, setFormData] = useState({
+    fullName: "",
+    userEmail: "",
+    userPassword: "",
+    confirmPassword: "",
+  });
+
+  const [errors, setErrors] = useState({
+    errorFullName: "",
+    errorEmail: "",
+    errorPassword: "",
+    errorConfirmPassword: "",
+  });
+
+  const getPasswordError = (password) => {
+    const passwordRegex =
+      /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@.#$!%*?&])[A-Za-z\d@.#$!%*?&]{8,15}$/;
+
+    if (!passwordRegex.test(password)) {
+      if (password.length < 8 || password.length > 15) {
+        return "Password must contain 8 to 15 characters.";
+      } else {
+        return `Password must contain at least 1 lowecase,
+           1 uppercase ,1 digit and 1 special character.`;
+      }
+    } else {
+      return "";
+    }
+  };
+
+  const validateFormData = () => {
+    let newErrorValue = {
+      errorFullName: "",
+      errorEmail: "",
+      errorPassword: "",
+      errorConfirmPassword: "",
+    };
+    const nameRegex = /^[a-zA-Z\s]{2,50}$/;
+    const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+
+    if (!emailRegex.test(formData.userEmail)) {
+      newErrorValue.errorEmail = "Please enter a valid email address";
+    }
+
+    if (!nameRegex.test(formData.fullName)) {
+      newErrorValue.errorFullName = "Please enter characters between 2 to 50";
+    }
+
+    const passwordErrorMessage = getPasswordError(formData.userPassword);
+    newErrorValue.errorPassword = passwordErrorMessage;
+
+    let passwordConfirmErrorMessage = getPasswordError(
+      formData.confirmPassword,
+    );
+
+    if (passwordConfirmErrorMessage == "") {
+      if (formData.confirmPassword !== formData.userPassword) {
+        passwordConfirmErrorMessage = "Passwords does not match";
+      } else {
+        passwordConfirmErrorMessage = "";
+      }
+    }
+    newErrorValue.errorConfirmPassword = passwordConfirmErrorMessage;
+
+    setErrors(newErrorValue);
+    return newErrorValue;
+  };
+
+  const handleChange = (event) => {
+    setFormData((currValue) => {
+      const newFormData = {
+        ...currValue,
+        [event.target.name]: event.target.value,
+      };
+      return newFormData;
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const newError = validateFormData();
+    if (Object.values(newError).every((error) => error === "")) {
+      //Data submitted
+      setFormData({
+        fullName: "",
+        userEmail: "",
+        userPassword: "",
+        confirmPassword: "",
+      });
+    }
+  };
+
   return (
     <div className="flex flex-col">
-      <form action="" className="flex flex-col gap-3">
+      <form action="" onSubmit={handleSubmit} className="flex flex-col gap-3">
         <Input
           id="fullName"
           label="Full name"
           type="text"
+          inputName="fullName"
           placeholder="Enter your name"
+          onInputChange={handleChange}
+          error={errors.errorFullName}
+          value={formData.fullName}
         />
 
         <Input
           id="user-email"
           label="Email"
           type="email"
+          inputName="userEmail"
           placeholder="Enter your email"
+          onInputChange={handleChange}
+          error={errors.errorEmail}
+          value={formData.userEmail}
         />
 
         <Input
           id="user-password"
           type="password"
           label="Password"
+          inputName="userPassword"
           placeholder="Enter your password"
+          onInputChange={handleChange}
+          error={errors.errorPassword}
+          value={formData.userPassword}
         />
 
         <Input
           id="confirm-password"
           label="Confirm Password"
           type="password"
+          inputName="confirmPassword"
           placeholder="Enter your password"
+          onInputChange={handleChange}
+          error={errors.errorConfirmPassword}
+          value={formData.confirmPassword}
         />
 
         <button
